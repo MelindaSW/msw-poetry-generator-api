48c4e738eb65090188a07fa106c439806d893da6
Melinda <sandstrommelinda@gmail.com>
2021-04-05 01:06:55 +0200

    Add cors configuration for poetry controller

--- src/main/java/se/melindasw/poetrygenerator/controllers/PoetryController.java
+++ src/main/java/se/melindasw/poetrygenerator/controllers/PoetryController.java
@@ -3,10 +3,13 @@ package se.melindasw.poetrygenerator.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import se.melindasw.poetrygenerator.acrostic.Acrostic;
 import se.melindasw.poetrygenerator.acrostic.AcrosticRequestModel;
+import se.melindasw.poetrygenerator.exceptions.AcrosticGeneratorException;
 import se.melindasw.poetrygenerator.exceptions.HaikuGeneratorException;
 import se.melindasw.poetrygenerator.haiku.Haiku;
 import se.melindasw.poetrygenerator.haiku.HaikuRequestModel;
@@ -26,30 +29,31 @@ public class PoetryController {
     this.acrosticService = acrosticService;
   }
 
+  @CrossOrigin
   @PostMapping("/haiku")
-  public ResponseEntity generateHaiku(@RequestBody final HaikuRequestModel model) {
+  public ResponseEntity<Object> generateHaiku(@RequestBody final HaikuRequestModel model) {
     Haiku generatedHaiku = null;
     try {
       generatedHaiku = haikuService.generateHaiku(model.getText());
     } catch (HaikuGeneratorException e) {
       e.printStackTrace();
-      return new ResponseEntity(
+      return new ResponseEntity<>(
           new HaikuGeneratorException(e.getMessage(), e.getErrorHaiku()), HttpStatus.BAD_REQUEST);
     }
-    return new ResponseEntity(generatedHaiku, HttpStatus.ACCEPTED);
+    return new ResponseEntity<>(generatedHaiku, HttpStatus.ACCEPTED);
   }
 
+  @CrossOrigin
   @PostMapping("/acrostic")
-  public ResponseEntity generateAcrosticPoem(@RequestBody final AcrosticRequestModel model) {
-    //        Acrostic generatedAcrostic = null;
-    //        try {
-    //            generatedAcrostic = acrosticService.generateAcrosticPoem("Test text", "T");
-    //        } catch (AcrosticGeneratorException e) {
-    //            e.printStackTrace();
-    //            return new ResponseEntity(new AcrosticGeneratorException(e.getMessage(),
-    // e.getAcrosticError()), HttpStatus.BAD_REQUEST);
-    //        }
-    //        return new ResponseEntity(generatedAcrostic, HttpStatus.ACCEPTED);
-    return new ResponseEntity("Feature in progress", HttpStatus.OK);
+  public ResponseEntity<Object> generateAcrosticPoem(@RequestBody final AcrosticRequestModel model) {
+            Acrostic generatedAcrostic = null;
+            try {
+                generatedAcrostic = acrosticService.generateAcrosticPoem("Test text", "T");
+            } catch (AcrosticGeneratorException e) {
+                e.printStackTrace();
+                return new ResponseEntity<>(new AcrosticGeneratorException(e.getMessage(),
+     e.getAcrosticError()), HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>(generatedAcrostic, HttpStatus.ACCEPTED);
   }
 }
